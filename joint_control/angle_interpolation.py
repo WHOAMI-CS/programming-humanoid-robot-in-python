1f2d4f4fb862f5d4c1b71aae98f28764969712bb
WHOAMI-CS <29006578+WHOAMI-CS@users.noreply.github.com>
2020-05-25 12:15:33 +0200

    Update angle_interpolation.py

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -32,6 +32,7 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.sTime = -1
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -41,7 +42,36 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        (names, times, keys) = keyframes
 
+        if(self.sTime == -1):
+            self.sTime = perception.time
+        time = perception.time - self.sTime
+        for j, joint_name in enumerate(names):
+            if joint_name not in self.joint_names:
+                continue
+            tJoint = times[j]
+            kJoint = keys[j]
+            for t in range(len(tJoint)):
+                if time > tJoint[-1]:  # when time > last item in the joints
+                    target_joints[joint_name] = perception.joint[joint_name]
+                    continue
+                if time < tJoint[t]:
+                    if t == 0:
+                        t0, P_0, P_1 = 0, 0, 0
+                        T_3 = tJoint[0]
+                        P_2 = kJoint[0][1][2]
+                        P_3 = kJoint[0][0]
+                        i = time / T_3
+                    elif time >= tJoint[t-1]:
+                        t0, T_3 = tJoint[t-1], tJoint[t]
+                        P_0, P_3 = kJoint[t-1][0], kJoint[t][0]
+                        P_1 = P_0 + kJoint[t-1][2][2]
+                        P_2 = P_3 + kJoint[t][1][2]
+                        i = (time - t0)/(T_3 - t0)
+                    target_joints[joint_name] = (
+                        (1-i)**3)*P_0 + 3*((1-i)**2)*i*P_1 + 3*((1-i)**3)*(i**2)*P_2 + (i**3)*P_3
+                    break
         return target_joints
 
 if __name__ == '__main__':
